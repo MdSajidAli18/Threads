d49b1dff2aa0cd38556c05ebd27eb3ee9718908d
Sajid <mdsajidali1803@gmail.com>
2024-04-26 14:30:52 +0530

    Edit file

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,7 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
-//import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 function Topbar() {
   return (
@@ -30,7 +30,7 @@ function Topbar() {
 
             <OrganizationSwitcher
                 appearance={{
-                    //baseTheme: dark,
+                    baseTheme: dark,
                     elements: {
                     organizationSwitcherTrigger: "py-2 px-4",
                     },
